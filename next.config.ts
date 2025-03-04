0c1296af7a84743027708759873641af3fa92746
n0wlk <kxwxn@icloud.com>
2025-03-04 23:54:40 +0900

    config changed

--- next.config.ts
+++ next.config.ts
@@ -12,18 +12,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  output: 'standalone',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(mp4|webm)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/videos/',
-          outputPath: 'static/videos/',
-          name: '[name].[ext]',
-          mimetype: 'video/mp4'
-        },
-      },
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/videos/[name][ext]'
+      }
     });
     return config;
   },
