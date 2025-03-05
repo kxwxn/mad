dd36ff592831d46e331fc74cf6d873ae6578fdc8
n0wlk <kxwxn@icloud.com>
2025-03-05 14:05:23 +0900

    updated

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,14 @@ const nextConfig: NextConfig = {
             key: 'Content-Type',
             value: 'video/mp4',
           },
+          {
+            key: 'Accept-Ranges',
+            value: 'bytes',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
         ],
       },
     ];
