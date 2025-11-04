5f8b5a2c51300ca94077215e066ea544401f78d2
n0wlk <kxwxn@icloud.com>
2025-11-04 17:08:54 +0900

    full image extension corrected

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
       />
 
       <FullImageSection
-        imageUrl="/Images/LandingPhoto/Mycelium.jpg"
+        imageUrl="/Images/LandingPhoto/Mycelium.jpeg"
         altText="Image 1"
         overlayText="We are not industry bound, we are addressing the urgent need for sustainable materials across different industries."
         topOverlayText="We develop custom solutions for your projects and products based on our unique recipes and IP. Get in touch!"
